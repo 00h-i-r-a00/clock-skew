cca13ff854cbfeaf6db57f2e7579bdab6ad81791
00h-i-r-a00 <15030004@lums.edu.pk>
2018-12-18 13:54:36 -0500

    changes to postgres

--- app/app.py
+++ app/app.py
@@ -6,20 +6,14 @@ from sqlalchemy import exc
 from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku``
 
-project_dir = os.path.dirname(os.path.abspath(__file__))
-
-#path to the database
-
-#database_file = "postgresql:///{}".format(os.path.join(project_dir, "userdatabase.db"))
-
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #indicate to the web application where the database will be stored
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://favlpjtwngyxmo:16331a96a43a27f46cd02cf9af66a89f9fa07594ddc5b2c2fbc168b2df38b05a@ec2-23-21-122-141.compute-1.amazonaws.com:5432/d7vhvov24eqmvt'
+postgres_url = 'postgres://favlpjtwngyxmo:16331a96a43a27f46cd02cf9af66a89f9fa07594ddc5b2c2fbc168b2df38b05a@ec2-23-21-122-141.compute-1.       amazonaws.com:5432/d7vhvov24eqmvt'
+app.config["SQLALCHEMY_DATABASE_URI"] = postgres_url
 #initialize a connection to the database; use the db variable to interact with the database
-#db = SQLAlchemy(app)
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 db.init_app(app)
 db.app = app
 db.create_all()
@@ -55,7 +49,6 @@ def home():
     users = User.query.all()
 
     return render_template("home.html", users=users)
-
 
 
 if __name__ == "__main__":
